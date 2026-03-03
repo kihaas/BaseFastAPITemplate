4e55b41d4df576ff1e490bbe27922b5572f76ded
kihaas <qwkihaas@gmail.com>
2026-03-03 20:54:50 +0400

    feat:work

--- main.py
+++ main.py
@@ -68,6 +68,7 @@ import uvicorn
 
 from items_views import router as items_router
 from users.views import router as users_router
+
 app = FastAPI()
 app.include_router(items_router)
 app.include_router(users_router)
@@ -75,9 +76,7 @@ app.include_router(users_router)
 
 @app.get("/")
 def hello():
-    return {
-        "message": "Hello World!"
-    }
+    return {"message": "Hello World!"}
 
 
 @app.get("/hello")
@@ -85,38 +84,15 @@ def hello(name: str = "World"):
     name = name.strip().title()
     return {"message": f"hello,{name}"}
 
+
 @app.get("/calc")
-def calculator(a: float, b:float):
-    return{
+def calculator(a: float, b: float):
+    return {
         "a": a,
         "b": b,
-        "result": a+b,
+        "result": a + b,
     }
-
-
-
 
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- users/views.py
+++ users/views.py
@@ -6,6 +6,7 @@ from users.schemas import CreateUser
 
 router = APIRouter(prefix="/users", tags=["users"])
 
+
 @router.post("/")
 def create_user(user: CreateUser):
     return crud.create_user(user_in=user)
