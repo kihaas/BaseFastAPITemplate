cf67cf3f81ce1114f00811f0e269e93ab61b09f2
kihaas <qwkihaas@gmail.com>
2026-03-03 22:15:39 +0400

    feat: models

--- models/product.py
+++ models/product.py
@@ -1,5 +1,8 @@
+from sqlalchemy.orm import Mapped
+from sqlalchemy.testing.schema import mapped_column
+
 from .base import Base
 
 
 class Product(Base):
-    pass
+    id: Mapped[int] = mapped_column(primary_key=True)
